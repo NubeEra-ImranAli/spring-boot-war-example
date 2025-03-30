93c021dad939bf9e05ae620913ac7b942bfa1b28
Imran Ali Khan <114231155+NubeEra-ImranAli@users.noreply.github.com>
2025-03-30 13:07:10 +0530

    main.tf one server only for not cry too

--- main.tf
+++ main.tf
@@ -10,30 +10,7 @@ resource "aws_instance" "build_server" {
     Name = "Build-Server"
   }
 }
-resource "aws_instance" "tomcat_server" {
-  ami           = "ami-084568db4383264d4"
-  instance_type = "t2.micro"
-  security_groups = ["sgalltraffic"]  # Reference the existing security group
-  key_name      = "mujahed"
-  tags = {
-    Name = "Tomcat-Server"
-  }
-}
-resource "aws_instance" "artifact_server" {
-  ami           = "ami-084568db4383264d4"
-  instance_type = "t2.micro"
-  security_groups = ["sgalltraffic"]  # Reference the existing security group
-  key_name      = "mujahed"
-  tags = {
-    Name = "Artifact-Server"
-  }
-}
+
 output "build_server_ip" {
   value = aws_instance.build_server.public_ip
-}
-output "tomcat_server_ip" {
-  value = aws_instance.tomcat_server.public_ip
-}
-output "artifact_server_ip" {
-  value = aws_instance.artifact_server.public_ip
 }
