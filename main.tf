f1c7a0aaeeda75d081fc9a0bd460037cc86ca02d
Imran Ali Khan <114231155+NubeEra-ImranAli@users.noreply.github.com>
2025-04-02 18:58:19 +0530

    Update main.tf---- final

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "build_server" {
-  ami           = "ami-084568db4383264d4"  # Ubuntu 20.04 AMI
+  ami           = "ami-0c55b159cbfafe1f0"  # Ubuntu 20.04 AMI
   instance_type = "t2.micro"
   security_groups = ["sgalltraffic"]  # Reference the existing security group
   key_name      = "mujahed"
@@ -10,7 +10,30 @@ resource "aws_instance" "build_server" {
     Name = "Build-Server"
   }
 }
-
+resource "aws_instance" "tomcat_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  security_groups = ["sgalltraffic"]  # Reference the existing security group
+  key_name      = "mujahed"
+  tags = {
+    Name = "Tomcat-Server"
+  }
+}
+resource "aws_instance" "artifact_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  security_groups = ["sgalltraffic"]  # Reference the existing security group
+  key_name      = "mujahed"
+  tags = {
+    Name = "Artifact-Server"
+  }
+}
 output "build_server_ip" {
   value = aws_instance.build_server.public_ip
+}
+output "tomcat_server_ip" {
+  value = aws_instance.tomcat_server.public_ip
+}
+output "artifact_server_ip" {
+  value = aws_instance.artifact_server.public_ip
 }
