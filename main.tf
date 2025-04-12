70aa36b4826e316faa8fba2d5f1df6f3d08baf5b
Imran Ali Khan <114231155+NubeEra-ImranAli@users.noreply.github.com>
2025-04-12 10:06:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,18 +10,7 @@ resource "aws_instance" "tomcat_server" {
     Name = "Tomcat-Server"
   }
 }
-resource "aws_instance" "artifact_server" {
-  ami           = "ami-084568db4383264d4"
-  instance_type = "t2.micro"
-  security_groups = ["sgalltraffic"]  # Reference the existing security group
-  key_name      = "mujahed"
-  tags = {
-    Name = "Artifact-Server"
-  }
-}
+
 output "tomcat_server_ip" {
   value = aws_instance.tomcat_server.public_ip
-}
-output "artifact_server_ip" {
-  value = aws_instance.artifact_server.public_ip
 }
